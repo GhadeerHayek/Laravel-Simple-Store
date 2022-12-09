7fc423efbd0045ec158652fc71cfb10fb7766e79
Ghadeer Alhayek <ghadeerhayek2001@gmail.com>
2022-12-09 10:02:20 +0200

    fix: re-structured website/index so that blade directives could be included

--- resources/views/layouts/website_main_layout.blade.php
+++ resources/views/layouts/website_main_layout.blade.php
@@ -1,1 +1,31 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- basic -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- mobile metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+    <!-- site metas -->
+    <title>Eflyer</title>
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    @include('includes.website_includes.css_includes')
+</head>
+<body>
+<!-- banner bg main start -->
+@include('includes.website_includes.banner')
+    <!-- banner bg main end -->
+    @yield('PageContent')
+    <!-- footer section start -->
+    @include('includes.website_includes.footer')
+    <!-- footer section end -->
+    <!-- copyright section start -->
+    @include('includes.website_includes.copyright')
+    <!-- copyright section end -->
+@include('includes.website_includes.js_includes')
+</body>
+</html>
